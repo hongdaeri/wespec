cb3cf1cb1e505eb748f27eda8e4bb65dfd5e35a0
h0ngz <h0ngz@naver.com>
2014-11-05 22:39:12 +0900

    컨넥션풀 설정 + JDBCUtil
    컨넥션풀 사용 적용해야함 내일

--- WESPEC/src/jdbc/loader/DBCPInit.java
+++ WESPEC/src/jdbc/loader/DBCPInit.java
@@ -1,14 +1,22 @@
 package jdbc.loader;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
+import javax.sql.DataSource;
+
+import com.mysql.jdbc.Connection;
+
 import java.util.StringTokenizer;
 
 public class DBCPInit extends HttpServlet {
 
+
 	public void init(ServletConfig config) throws ServletException {
 		try {
+			
 			String drivers = config.getInitParameter("jdbcdriver");
 			StringTokenizer st = new StringTokenizer(drivers, ",");
 			while (st.hasMoreTokens()) {
--- WESPEC/src/jdbc/util/JdbcUtil.java
+++ WESPEC/src/jdbc/util/JdbcUtil.java
@@ -1,49 +1,63 @@
 package jdbc.util;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
 public class JdbcUtil {
+	
+	public static Connection getConnection(Connection conn)
+	{		
+		try {
+			Context initContext = new InitialContext();
+			Context envContext = (Context) initContext.lookup("java:/comp/env");
+			DataSource dataSource = (DataSource) envContext.lookup("jdbc/mysql");
+			conn = dataSource.getConnection();
+		} catch (NamingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}		
+		return conn;		
+	}
+	
+	
+	public static void close(ResultSet rs, PreparedStatement pstmt, Connection conn)
+	{
+		if(rs!=null) { try{ rs.close(); } catch(Exception ex) { } } 
+		if(pstmt!=null) { try{ pstmt.close(); } catch(Exception ex) { } } 
+		if(conn!=null) { try{ conn.close(); } catch(Exception ex) { } } 
+	}
+	
+	public static void close(PreparedStatement pstmt, Connection conn)
+	{
+		
+		if(pstmt!=null) { try{ pstmt.close(); } catch(Exception ex) { } } 
+		if(conn!=null) { try{ conn.close(); } catch(Exception ex) { } } 
+	}
 
 	public static void close(ResultSet rs) {
-		if (rs!=null) {
-			try {
-				rs.close();
-			} catch (SQLException ex) {
-
-			}
-		}
+		if (rs!=null) { try { rs.close(); } catch (SQLException ex) { }	}
 	}
 
 	public static void close(Statement stmt) {
-		if (stmt!=null) {
-			try {
-				stmt.close();
-			} catch (SQLException ex) {
-
-			}
-		}
+		if (stmt!=null) { try {	stmt.close();} catch (SQLException ex) {}}
 	}
 
 	public static void close(Connection conn) {
-		if (conn!=null) {
-			try {
-				conn.close();
-			} catch (SQLException ex) {
-
-			}
-		}
+		if (conn!=null) {try {	conn.close();} catch (SQLException ex) {}}
 	}
 
 	public static void rollback(Connection conn) {
-		if (conn!=null) {
-			try {
-				conn.rollback();
-			} catch (SQLException ex) {
-
-			}
-		}
+		if (conn!=null) {	try {	conn.rollback(); } catch (SQLException ex) {}}
 	}
 }
